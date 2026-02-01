4c77d90fe537807f2b83dbb0f2a4acb467fd5fde
BluePigman <82780692+BluePigman@users.noreply.github.com>
2026-02-01 13:12:13 -0500

    Update login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useSearchParams } from "react-router-dom";
 import MapView from "./components/MapView";
 import AuthCallback from "./components/AuthCallback";
+
+function RootHandler() {
+  const [searchParams] = useSearchParams();
+  const code = searchParams.get("code");
+
+  if (code) {
+    return <AuthCallback />;
+  }
+  return <MapView />;
+}
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MapView />} />
+        <Route path="/" element={<RootHandler />} />
         <Route path="/auth/callback" element={<AuthCallback />} />
       </Routes>
     </Router>
